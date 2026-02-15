5851879529b5b6b43474dbfed8c858ba18125aa4
kalwa2407 <talukdaraman24@gmail.com>
2026-02-15 17:39:59 +0530

    Fix next.config.mjs invalid keys and skip linting in build script

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: '.next',
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
